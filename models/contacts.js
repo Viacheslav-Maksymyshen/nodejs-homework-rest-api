ebb3c0bba763229a75a6291817abbc68063821be
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-12-04 14:53:27 +0200

    hm02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,65 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.resolve("models/contacts.json");
 
-const listContacts = async () => {}
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const DataPars = JSON.parse(data);
+    return DataPars;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-const getContactById = async (contactId) => {}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId.toString());
+    return result || null;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-const removeContact = async (contactId) => {}
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(
+      (item) => item.id === contactId.toString()
+    );
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
