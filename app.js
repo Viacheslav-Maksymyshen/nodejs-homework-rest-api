b43043a99076826d4d6dd3a923f4fa595d49de48
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-12-10 22:00:13 +0200

    added db

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Success connect to db"))
+  .catch((error) => console.log(error.message));
+
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
