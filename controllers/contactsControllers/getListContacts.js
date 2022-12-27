126fb8cfe79ef56bc9ea5136638c80a477f3b845
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-12-27 13:47:03 +0200

    added favorite

--- controllers/contactsControllers/getListContacts.js
+++ controllers/contactsControllers/getListContacts.js
@@ -3,10 +3,17 @@ const { Contact } = require("../../models/contact");
 const getListContacts = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite = true } = req.query;
     const skip = (page - 1) * limit;
 
-    const result = await Contact.find({ owner }, "", { skip, limit });
+    const result = await Contact.find(
+      { owner, favorite: favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    );
 
     res.json(result);
   } catch (error) {
