2b07ffe8d99410b87dfc3e8e364645b2d980f9b2
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-12-27 20:39:27 +0200

    added  token validation

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,20 +5,23 @@ const { User } = require("../models/users");
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
+  if (!authorization) {
+    next(HttpError(401, "Not authorized 1"));
+  }
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    next(HttpError(401, "Not authorized 1"));
+    next(HttpError(401, "Not authorized 2"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user) {
-      next(HttpError(401, "Not authorized 2"));
+      next(HttpError(401, "Not authorized 3"));
     }
     req.user = user;
     next();
   } catch {
-    next(HttpError(401, "Not authorized 3"));
+    next(HttpError(401, "Not authorized 4"));
   }
 };
 
